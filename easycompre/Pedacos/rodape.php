7b202c243b2ead01560a4498f54658a4728d6b6c
brunogeek9 <brunogeek9@gmail.com>
2015-12-21 16:45:05 -0300

    Revert "Meu Primeiro commit"
    This reverts commit 5db0d776a21c9f78df888d067798c1a36b324ac6.

--- easycompre/Pedacos/rodape.php
+++
@@ -1,8 +0,0 @@
-<footer>
-	<div id="contato">
-		<h6>Desenvolvido por Bruno Jamelle e Hanna leticia</h6>
-		<img src="imagens/fb.png" id="face">
-		<img src="imagens/tw.png" id="tw">
-		<img src="imagens/g+.png" id="g">
-	</div>
-</footer>
