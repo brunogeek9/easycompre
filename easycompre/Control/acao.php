7649c92c65943ab2c178d31d905d29d96d3bb558
brunogeek9 <brunogeek9@gmail.com>
2016-01-15 09:01:11 -0300

    finalizando compra e removendo protudos

--- easycompre/Control/acao.php
+++ easycompre/Control/acao.php
@@ -4,68 +4,63 @@
 	include_once("lib.php");
 	class acao{
 		public function AbrePagina(){
-		$acao=$_GET['acao'];
+					$acao=$_GET['acao'];
 
-		if(isset($_GET['acao'])){
-		$id=$_GET['id'];	
-        if($acao == 'telaProd'){
-          $ob = new controladorProdutos();
-          $ob-> GerarTela($id);
-		}
-
-		if($acao == 'frete'){
-  			$cep = $_POST['cep'];
-  			$peso = 1;
-  			//echo \Metodos\PHPAlert("a");
-
-  			header('location: http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=&sDsSenha=&sCepOrigem=59200000&sCepDestino='.$cep.'&nVlPeso='.$peso.'&nCdFormato=1&nVlComprimento=20&nVlAltura=5&nVlLargura=15&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=41106&nVlDiametro=0&StrRetorno=http://easycompre.esy.es/paginas.php?acao=telaCar'); 
-
-		}
-		if($acao=='FinalizarCompra'){
-			//die("morrrreu");
-			include_once("pedido.php");
-		}
-		if($acao=='add'){
-          $ob = new Carrinho;
-          $id=$_GET['id'];
-          $ob->AdicionarProduto($id);
-          $ob->TelaCarrinho();
-		}
-		if($acao=='rm'){
-          $dog=new Carrinho;
-          $id=$_GET['id'];
-          $dog->RemoverProduto($id);
-		  $dog->TelaCarrinho();
-        }
-        if($acao=='alterar'){
-          //if (isset($_SESSION['carrinho'][$id])){
-			if (isset($_POST['decremento'])){
-							 
-					echo "haha";
-					$_SESSION['carrinho'][$id] -= 1;
-					//$dog= new Carrinho;
-					//$dog->TelaCarrinho();
-					if ($_SESSION['carrinho'][$id] == 0){
-						unset($_SESSION['carrinho'][$id]);
+					if(isset($_GET['acao'])){
+					$id=$_GET['id'];	
+			        if($acao == 'telaProd'){
+			          $ob = new controladorProdutos();
+			          $ob-> GerarTela($id);
 					}
-		  }
-		  elseif (isset($_POST['incremento'])) {
-				$_SESSION['carrinho'][$id] += 1;
-				$dog->TelaCarrinho();
+
+					if($acao == 'frete'){
+			  			$cep = $_POST['cep'];
+			  			$peso = 1;
+			  			//echo \Metodos\PHPAlert("a");
+			  			header('location: http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=&sDsSenha=&sCepOrigem=59200000&sCepDestino='.$cep.'&nVlPeso='.$peso.'&nCdFormato=1&nVlComprimento=20&nVlAltura=5&nVlLargura=15&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&nCdServico=41106&nVlDiametro=0&StrRetorno=http://easycompre.esy.es/paginas.php?acao=telacar'); 
+					}
+					if($acao=='FinalizarCompra'){
+						//die("morrrreu");
+						include_once("pedido.php");
+					}
+					if($acao=='add'){
+			          $ob = new Carrinho;
+			          $id=$_GET['id'];
+			          $ob->AdicionarProduto($id);
+			          $ob->TelaCarrinho();
+					}
+					if($acao=='rm'){
+			          $dog=new Carrinho;
+			          $id=$_GET['id'];
+			          $dog->RemoverProduto($id);
+					  $dog->TelaCarrinho();
+					  //include_once("paginas?acao=telacar");
+			        }
+			        if($acao=='alterar'){
+			          //if (isset($_SESSION['carrinho'][$id])){
+						if (isset($_POST['decremento'])){		 
+								echo "haha";
+								$_SESSION['carrinho'][$id] -= 1;
+								//$dog= new Carrinho;
+								//$dog->TelaCarrinho();
+								if ($_SESSION['carrinho'][$id] == 0){
+									unset($_SESSION['carrinho'][$id]);
+								}
+					  	}
+					    elseif (isset($_POST['incremento'])) {
+							$_SESSION['carrinho'][$id] += 1;
+							$dog->TelaCarrinho();
+						}
+					}	
+					if($acao=='telacar'){	
+						$dog=new Carrinho;
+						$dog->TelaCarrinho();
+					}
+			        if($acao=='telaCats'){
+        				$dog=new ControladorCat;
+            			$dog->TelaCat;
+		        }
 			}
-		}	
-		if($acao=='telacar'){
-			
-			$dog=new Carrinho;
-			//$dog->MudaQtd();
-			$dog->TelaCarrinho();
-		}
-                if($acao=='telaCats'){
-                        $dog=new ControladorCat;
-                        $dog->TelaCat;
-                }
-		
-		}
 		}
 	}
 ?>
--- easycompre/pedido.php
+++ easycompre/pedido.php
@@ -7,39 +7,48 @@
 	$dog = new PDO("mysql:host=$db_host;dbname=$db_name;
 	charset=utf8",$db_user,$db_senha);
 	$dog->beginTransaction();
+
+	$cod_referencia="14-14";
+	$data_pagamento=date('Y-m-d');
 	$data_pedido = date('Y-m-d');
-	$frete_pedido = $_POST['frete'];
-	$previsao_entrega="";
-	$data_pagamento="";
+	$frete_pedido = 12.5;//$_POST['frete'];
+	$previsao_entrega;//date('Y-m-d');
 	$status_pagamento = 'Aguardando Pagamento';
 	$status_pedido = 'Pedido Realizado';
-	$cpf = $_SESSION['cpf'];
-
-	$dog->exec("insert into pedido (data_pagamento,data_pedido,frete_pedido,previsao_entrega,status_pagamento,status_pedido,cpf) 
-	values ('$data_pagamento','$data_pedido',$frete_pedido,$previsao_entrega,'$status_pagamento','$status_pedido','$cpf')");
+	$cpf = '01755644428';//$_SESSION['cpf'];
+	//teste PDO
+	$dog->exec("insert into pedido (cod_referencia,data_pagamento,data_pedido,frete_pedido,previsao_entrega,status_pagamento,status_pedido,cpf) 
+	values ('$cod_referencia','$data_pagamento','$data_pedido',$frete_pedido,'$previsao_entrega','$status_pagamento','$status_pedido','$cpf')");
+	$ultimoID=$dog->lastInsertId(); 	
 	foreach ($_SESSION['carrinho'] as $id=>$qtd) {
-		$ultimo = $dog->insert_id;
+		
 		$consulta=$dog->prepare("select preco from produto where id_produto=$id");
 		$consulta->execute();
 		$linha=$consulta->fetch();
 		$preco_unit=$linha[0];
+		//echo $preco_unit;
+		echo $ultimo;
 		$dog->exec("insert into pedido_produto (id_pedido,id_produto,preco_unit,quantidade) 
-	values ($ultimo,$id,$preco_unit,$qtd)");
-		$dog->exec("update produto set unidades=unidades-$qtd where id=$id");
-		$consulta1 = $dog->prepare ( "select unidades from produto 
-	where id = $id" );
+		values ($ultimoID,$id,$preco_unit,$qtd)");
+		$dog->exec("update produto set unidades=unidades-$qtd where id_produto=$id");
+		$consulta1 = $dog->prepare("select unidades from produto where id_produto = $id");
+		$consulta1->execute();
 		$linha1=$consulta1->fetch();
-		if($linha1[0]<0){
+		echo $linha1[0].'<br/>';
+		$numero=$linha1[0];
+		if($numero<0){
+			echo "numero de produtos insuficiente";
 			$dog->rollBack ();
-			exit();
+			die();
 		}
-		
-		
 	}
-$cop=$dog->commit();
-if($cop){
-			echo "venda efetuada com sucesso";
-		}
+	
+	$cop=$dog->commit();
+	
+
+	if($cop){
+		echo "venda efetuada com sucesso";
+	}
 
 
 	//consultar se quantidade ficou negativa
