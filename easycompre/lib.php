4c29258de9f5835b873ebc1240d31400220ad952
brunogeek9 <brunogeek9@gmail.com>
2016-01-18 00:24:24 -0300

    Tratamento de dados
    dados de cadastro de usuario tratados,dados como
    data,senha,nome,telefone,etc

--- easycompre/lib.php
+++ easycompre/lib.php
@@ -4,7 +4,15 @@
 		echo '<script type="text/javascript"> alert('.$msg.');</script>';
 	}
 
-	function limparCPF($objeto){
+	function validarNome($nome){
+		if((strlen($nome)>8)&&($nome<40)){
+			return true;
+		}
+		else
+			return false;	
+	}
+
+	function limparDado($objeto){
 		for($i = 0; $i<strlen($objeto);$i++){
 			if(($objeto[$i] >= '0') && ($objeto[$i]<='9'))
 			$objl = $objl.$objeto[$i]; 
@@ -35,5 +43,44 @@
 			}
 			} else {
 			}
-}
+	}
+	
+	function validarTelefone($telefone){
+		$telefone = str_replace("(", "", $telefone);
+		$telefone = str_replace(")", "", $telefone);
+		$telefone = str_replace(" ", "", $telefone);
+		if((strlen($telefone)==11)&&(is_numeric($telefone))){
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+	function validarData($data){
+		$dataL=limparDado($data);
+		$hoje=date('Y-m-d');
+		if(count($dataL)==8){
+			$dia= $dataL[0].$dataL[1];
+			$mes = $dataL[2].$dataL[3];
+			$ano= $dataL[4].$dataL[5].$dataL[6].$dataL[7];
+			if(checkdate($mes, $dia, $ano)){
+				$ha=$hoje[0].$hoje[1].$hoje[2].$hoje[3];
+				$hm=$hoje[5].$hoje[6];
+				$hd=$hoje[8].$hoje[9];
+				if (($ano>$ha+18)&&($mes>=$hm)&&($dia>=$hd)) {
+					return $dataL;
+				}
+			}
+		}
+		else
+			return false;
+	}
+
+	function validarSenha($senha,$confirmar){
+		if(($senha==$confirmar)&&(strlen($senha)>=8)&&(strlen($senha)<=40)&&(strlen($confirmar)>=8)&&(strlen($confirmar)<=40)){
+			return true;
+		}
+		else 
+			return false;
+	}
 ?>
